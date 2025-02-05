a24fc0f27e28718a71fae8e63b555f6da2796dfa
Brandon Gutierrez <br4andong@gmail.com>
2025-02-04 20:50:47 -0600

    aaa

---
+++ Proceso.py
@@ -0,0 +1,9 @@
+class Proceso:
+    def __init__(self):
+        self.operacion = (" ")
+        self.numero1 = ( )
+        self.numero2 = ( )
+        self.tiempo_maximo = (" ")
+        self.id_programa = ( )
+        self.resultado_operacion = ( )
+        self.cadena_operacion = (" ")
